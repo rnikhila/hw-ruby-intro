6695a6dc7983764c140dec6f0af95df2d1fce365
EC2 Default User <ec2-user@ip-172-31-1-191.us-east-2.compute.internal>
2019-01-31 02:49:45 +0000

    added part 1 functions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,34 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each do |i|
+    sum += i
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.sort
+  if arr.size >= 2
+    return arr[-2]+arr[-1]
+  elsif arr.size = 0
+    return 0
+  else
+    return arr[-1]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  map = []
+  arr.each do |i|
+    if map.inclue ? (i)
+      return true
+    else
+      map.push(n-i)
+  return false
 end
 
 # Part 2
