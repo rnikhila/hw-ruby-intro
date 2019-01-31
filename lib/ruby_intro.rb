2249c8fbfa36f3c6322a1e7a4fb98231103de472
EC2 Default User <ec2-user@ip-172-31-1-191.us-east-2.compute.internal>
2019-01-31 03:40:03 +0000

    added part 2 functions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -27,29 +27,53 @@ def sum_to_n? arr, n
   # YOUR CODE HERE
   map = []
   arr.each do |i|
-    if map.inclue ? (i)
-      return true
-    else
       map.push(n-i)
-  return false
+  end
+  result = map & arr 
+  if result.size == 0
+    return false
+  else
+    return true
+  end
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  temp = "Hello, "
+  return temp+name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s[0] == "a" || s[0] == "e" ||s[0] == "i" ||s[0] == "o" || s[0] == "u"
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.length <2
+    return false
+  end
+  s.split("").each do |i|
+    if i != "1" || i != "0" 
+      return false
+    end
+    if s[-1] == 1 && s[-2] == 1
+      return true
+    else 
+      return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+
 end
